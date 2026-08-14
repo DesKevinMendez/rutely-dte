2f9a5f0ec3da80ef7e078e71cdb63ce9555b88c7
Kevin Méndez <kevin_mendez30@outlook.com>
2026-08-14 17:47:50 -0600

    style: 💄 (vue) removed all dependencies with innertia

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,10 +1,1 @@
-import { createInertiaApp } from '@inertiajs/vue3';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
-
-createInertiaApp({
-    title: (title) => (title ? `${title} - ${appName}` : appName),
-    progress: {
-        color: '#4B5563',
-    },
-});
+console.log('Hola')
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Welcome')->name('home');
+Route::get('/', fn () => view('app'))->name('home');
