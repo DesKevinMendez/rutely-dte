605b6da99f93595a6b4b1a4cbde48e0752050d8c
Kevin Mendez <32910223+DesKevinMendez@users.noreply.github.com>
2026-08-18 17:55:44 -0600

    feat: validate company creation request

--- app/Http/Requests/Company/StoreCompanyRequest.php
+++ app/Http/Requests/Company/StoreCompanyRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests\Company;
 
+use App\Models\Company;
 use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -12,7 +13,7 @@ class StoreCompanyRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return $this->user()?->can('create', Company::class) ?? false;
     }
 
     /**
@@ -23,7 +24,22 @@ class StoreCompanyRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'min:8', 'max:30'],
+            'nit' => ['required', 'string', 'regex:/^([0-9]{14}|[0-9]{9})$/'],
+            'nrc' => ['nullable', 'string', 'regex:/^[0-9]{2,8}$/'],
+            'commercial_name' => ['required', 'string', 'max:150'],
+            'economic_activity_code' => ['required', 'string', 'exists:economic_activities,code'],
+            'establishment_type' => ['required', 'string', 'exists:establishment_types,code'],
+            'departament_id' => ['required', 'uuid', 'exists:departaments,id'],
+            'municipality_id' => ['required', 'uuid', 'exists:municipalities,id'],
+            'district_id' => ['nullable', 'uuid', 'exists:districts,id'],
+            'email' => ['required', 'email', 'max:255'],
+            'mh_establishment_code' => ['required', 'string', 'size:4'],
+            'mh_pos_code' => ['required', 'string', 'size:4'],
+            'own_establishment_code' => ['required', 'string', 'size:4'],
+            'own_pos_code' => ['required', 'string', 'size:4'],
         ];
     }
 }
