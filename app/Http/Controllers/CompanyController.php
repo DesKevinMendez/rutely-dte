9da7884259d8401ebfd03dafa2d62f9808d7086b
Kevin Mendez <32910223+DesKevinMendez@users.noreply.github.com>
2026-08-18 23:20:26 -0600

    Complete REST company endpoints

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -4,34 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Company\StoreCompanyRequest;
 use App\Http\Requests\Company\UpdateCompanyRequest;
-use App\Http\Resources\CommonResource;
 use App\Models\Company;
 use App\Models\User;
-use Illuminate\Http\JsonResponse;
+use App\Response\CommonResponse;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Gate;
 
 class CompanyController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreCompanyRequest $request): JsonResponse
+    public function store(StoreCompanyRequest $request): CommonResponse
     {
         /** @var User $user */
         $user = $request->user();
@@ -47,42 +28,20 @@ class CompanyController extends Controller
             return $company;
         });
 
-        $company->refresh();
-
-        return CommonResource::make($company)
-            ->response()
-            ->setStatusCode(201);
+        return new CommonResponse(['data' => $company->refresh()], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Company $company)
+    public function show(Company $company): CommonResponse
     {
-        //
+        Gate::authorize('view', $company);
+
+        return new CommonResponse(['data' => $company]);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Company $company)
+    public function update(UpdateCompanyRequest $request, Company $company): CommonResponse
     {
-        //
-    }
+        $company->update($request->validated());
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateCompanyRequest $request, Company $company)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Company $company)
-    {
-        //
+        return new CommonResponse(['data' => $company->refresh()]);
     }
 }
