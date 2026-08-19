d888fd713b7178096b97b5e701b543ed224891fe
Kevin Mendez <32910223+DesKevinMendez@users.noreply.github.com>
2026-08-18 23:27:17 -0600

    Define DTE correlative persistence fields

--- app/Models/DteCorrelative.php
+++ app/Models/DteCorrelative.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
+#[Fillable(['company_id', 'key', 'last_value'])]
 class DteCorrelative extends Model
 {
     /** @use HasFactory<\Database\Factories\DteCorrelativeFactory> */
