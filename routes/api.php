6583166e73e47b3c008dcba1d0d6bd73b54e149e
Kevin Mendez <32910223+DesKevinMendez@users.noreply.github.com>
2026-08-18 23:21:55 -0600

    Register company and user API routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\auth\LoginController;
 use App\Http\Controllers\auth\RegisterController;
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CompanyEnvironmentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +18,19 @@ Route::group(['prefix' => 'v1'], function () {
     Route::post('register', RegisterController::class)->name('register');
     Route::post('login', LoginController::class)->name('login');
 
-    Route::post('companies', [CompanyController::class, 'store'])
-        ->middleware('auth:sanctum')
-        ->name('api.v1.companies.store');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('companies', [CompanyController::class, 'store'])
+            ->name('api.v1.companies.store');
+        Route::get('companies/{company}', [CompanyController::class, 'show'])
+            ->name('api.v1.companies.show');
+        Route::match(['put', 'patch'], 'companies/{company}', [CompanyController::class, 'update'])
+            ->name('api.v1.companies.update');
+        Route::get('companies/{company}/environment', [CompanyEnvironmentController::class, 'show'])
+            ->name('api.v1.companies.environment.show');
+        Route::match(['put', 'patch'], 'companies/{company}/environment', [CompanyEnvironmentController::class, 'update'])
+            ->name('api.v1.companies.environment.update');
+
+        Route::apiResource('users', UserController::class)
+            ->names('api.v1.users');
+    });
 });
