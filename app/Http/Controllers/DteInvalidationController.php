3c695670d241fc2b4efea4a801c60fa4aa145ae0
Kevin Mendez <32910223+DesKevinMendez@users.noreply.github.com>
2026-08-18 23:30:42 -0600

    Add REST DTE invalidation endpoint

--- app/Http/Controllers/DteInvalidationController.php
+++ app/Http/Controllers/DteInvalidationController.php
@@ -3,64 +3,25 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Dte\StoreDteInvalidationRequest;
-use App\Http\Requests\Dte\UpdateDteInvalidationRequest;
-use App\Models\DteInvalidation;
+use App\Models\Dte;
+use App\Response\CommonResponse;
+use App\Services\Dte\DteInvalidationService;
 
 class DteInvalidationController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
+    public function store(
+        StoreDteInvalidationRequest $request,
+        Dte $dte,
+        DteInvalidationService $invalidationService,
+    ): CommonResponse {
+        $result = $invalidationService->invalidate($dte, $request->validated());
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreDteInvalidationRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(DteInvalidation $dteInvalidation)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(DteInvalidation $dteInvalidation)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateDteInvalidationRequest $request, DteInvalidation $dteInvalidation)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(DteInvalidation $dteInvalidation)
-    {
-        //
+        return new CommonResponse([
+            'data' => [
+                'dte' => $result['updated'],
+                'invalidation' => $result['invalidation'],
+                'mh_result' => $result['mhResult'],
+            ],
+        ]);
     }
 }
